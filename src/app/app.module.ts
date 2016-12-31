c89f8dc8c5b1daf3391f22babbc025847f3d07d8
Swagata Prateek <thehoneymad@hotmail.com>
2017-01-01 00:29:14 +0100

    Added http module to the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
 
 import { APP_PROVIDERS } from './app.providers';
 import { AppComponent } from './app.component';
@@ -14,6 +15,7 @@ import { AuthGuard } from './auth/auth.guard';
     ],
     imports: [
         BrowserModule,
+        HttpModule,
         HomeModule,
         LoginModule,
         routing
