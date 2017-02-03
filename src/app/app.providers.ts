0ae01cebef85164792b67d4b4d5eb48a9b7ee24c
Swagata Prateek <thehoneymad@hotmail.com>
2017-02-03 23:25:37 +0100

    Added a simple logging service for enterprise dash

--- src/app/app.providers.ts
+++ src/app/app.providers.ts
@@ -1,7 +1,8 @@
-import { LOCAL_STORAGE_PROVIDERS } from './shared';
+import { LOCAL_STORAGE_PROVIDERS, LOGGER_PROVIDERS } from './shared';
 import { AuthGuard } from './auth/auth.guard';
 
 export const APP_PROVIDERS = [
     AuthGuard,
-    ...LOCAL_STORAGE_PROVIDERS
+    ...LOCAL_STORAGE_PROVIDERS,
+    ...LOGGER_PROVIDERS
 ];
