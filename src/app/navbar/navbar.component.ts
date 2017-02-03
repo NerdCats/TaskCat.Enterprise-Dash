5cf9109bb1f9efb21623bcabd208ae71b81c0ef3
Swagata Prateek <thehoneymad@hotmail.com>
2017-02-04 00:21:52 +0100

    Started adding a navbar component

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     moduleId: module.id,
-    selector: 'as-navbar'
+    selector: 'as-navbar',
+    templateUrl: 'navbar.html'
 })
 export class NavbarComponent {}
