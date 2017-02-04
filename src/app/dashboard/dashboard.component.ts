1891b20c3aeab38cf12c9afda48c6cf386a4187a
Swagata Prateek <thehoneymad@hotmail.com>
2017-02-04 02:12:04 +0100

    Unused code cleanup from dashboard component

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,45 +1,8 @@
 import { Component } from '@angular/core';
-import { LocalStorage } from '../shared/index';
-import { AuthConstants } from '../auth/index';
-import { CONSTANTS } from '../shared/index';
 
 @Component({
     moduleId: module.id,
     selector: 'as-dashboard',
     templateUrl: 'dashboard.html'
 })
-export class DashboardComponent {
-    public avatarData: any = {
-        size: 40,
-        background: '#008d4c', // by default it will produce dynamic colors
-        fontColor: '#FFFFFF',
-        isSquare: false,
-        fixedColor: true
-    };
-
-    public userInfo: any = {};
-    public companyInfo: { title: string, url: string; tagline: string; };
-
-    public productInfo: { platform_title: string; product_title: string };
-
-    /**
-     * Dashborad container component for TaskCat.Enterprise
-     */
-    constructor(private localStorage: LocalStorage) {
-        let userToken = localStorage.getObject(AuthConstants.AUTH_TOKEN_KEY);
-        this.avatarData.text = userToken.userData.sub;
-        this.userInfo.Name = userToken.userData.sub;
-        this.userInfo.Email = userToken.email;
-
-        this.productInfo = {
-            platform_title: CONSTANTS.MAIN.APP.PLATFORM_TITLE,
-            product_title: CONSTANTS.MAIN.APP.PRODUCT_TITLE
-        };
-
-        this.companyInfo = {
-            title: CONSTANTS.MAIN.COMPANY.TITLE,
-            url: CONSTANTS.MAIN.COMPANY.URL,
-            tagline: CONSTANTS.MAIN.COMPANY.TAGLINE
-        };
-    }
-}
+export class DashboardComponent {}
