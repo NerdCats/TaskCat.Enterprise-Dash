44e509683ab067ed42ba262d218ff361038feb08
Swagata Prateek <thehoneymad@hotmail.com>
2017-02-04 13:53:36 +0100

    Making dashview-header component configurable

--- src/app/dashview-header/dashview-header.component.ts
+++ src/app/dashview-header/dashview-header.component.ts
@@ -5,4 +5,14 @@ import { Component } from '@angular/core';
     selector: 'as-dashview-header',
     templateUrl: 'dashview-header.html'
 })
-export class DashviewHeaderComponent { }
+export class DashviewHeaderComponent {
+    public pageHeader: string = 'Loading';
+    public optionalDescription: string = '';
+
+    /**INFO: Not sure how to play this out here.
+     * The first one has to be the dashboard logo
+     * and the last one has to have class=active on
+     * it so we know which one is active here
+     */
+    public breadcrumbDef: string[];
+}
